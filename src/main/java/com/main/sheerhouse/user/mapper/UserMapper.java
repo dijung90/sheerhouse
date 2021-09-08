3888e63768f5e79dbb20e28619c350a7825ecb7c
dijung90 <dijung90@daum.net>
2021-09-08 09:25:28 +0900

    회원가입 대충

--- src/main/java/com/main/sheerhouse/user/mapper/UserMapper.java
+++ src/main/java/com/main/sheerhouse/user/mapper/UserMapper.java
@@ -1,6 +1,13 @@
 package com.main.sheerhouse.user.mapper;
 
+import com.main.sheerhouse.user.domain.UserVO;
+
 public interface UserMapper {
 
+	//회원가입 ---------------------------------------------
+	//email체크 (실패시 email을 return 합니다)
+	public String emailCheck(String email);
+	//회원 등록 (성공 여부를 return 합니다)
+	public int insertUser(UserVO user);
 	
 }
--- src/main/java/com/main/sheerhouse/user/service/UserService.java
+++ src/main/java/com/main/sheerhouse/user/service/UserService.java
@@ -1,5 +1,12 @@
 package com.main.sheerhouse.user.service;
 
-public interface UserService {
+import com.main.sheerhouse.user.domain.UserVO;
 
+public interface UserService {
+	//회원가입 -------------------------------------------------
+	//이메일체크
+	public boolean emailCheck(String email);
+	//회원가입
+	public boolean insertUser(UserVO user);
+	
 }
